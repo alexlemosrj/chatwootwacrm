383a36f96f6c2f1aa9a784f1c3482a67b3655049
ALEXANDRE PEREIRA LEMOS <alexlemosrj.1971@gmail.com>
2026-09-21 00:12:24 -0300

    style(crm): use compact provisioning service namespace

--- app/services/crm/provision_account.rb
+++ app/services/crm/provision_account.rb
@@ -1,38 +1,36 @@
 # frozen_string_literal: true
 
-module Crm
-  class ProvisionAccount
-    def self.call(account)
-      new(account).call
-    end
+class Crm::ProvisionAccount
+  def self.call(account)
+    new(account).call
+  end
 
-    def initialize(account)
-      @account = account
-    end
+  def initialize(account)
+    @account = account
+  end
 
-    def call
-      account.with_lock do
-        pipeline = account.pipelines.find_by(is_default: true) || account.pipelines.order(:id).first
+  def call
+    account.with_lock do
+      pipeline = account.pipelines.find_by(is_default: true) || account.pipelines.order(:id).first
 
-        if pipeline.nil?
-          pipeline = account.pipelines.create!(name: 'Funil de Vendas', is_default: true)
-        elsif !pipeline.is_default?
-          pipeline.update!(is_default: true)
-        end
-
-        provision_default_stages(pipeline) if pipeline.pipeline_stages.empty?
-        pipeline
+      if pipeline.nil?
+        pipeline = account.pipelines.create!(name: 'Funil de Vendas', is_default: true)
+      elsif !pipeline.is_default?
+        pipeline.update!(is_default: true)
       end
+
+      provision_default_stages(pipeline) if pipeline.pipeline_stages.empty?
+      pipeline
     end
+  end
 
-    private
+  private
 
-    attr_reader :account
+  attr_reader :account
 
-    def provision_default_stages(pipeline)
-      Pipeline::DEFAULT_STAGES.each do |attributes|
-        pipeline.pipeline_stages.create!(attributes)
-      end
+  def provision_default_stages(pipeline)
+    Pipeline::DEFAULT_STAGES.each do |attributes|
+      pipeline.pipeline_stages.create!(attributes)
     end
   end
 end
