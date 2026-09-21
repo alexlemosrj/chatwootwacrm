092ad98d100f10736a26efb61222f768aa87c9b4
ALEXANDRE PEREIRA LEMOS <alexlemosrj.1971@gmail.com>
2026-09-20 23:06:13 -0300

    fix(crm): make deal event metadata Ruby 3 safe

--- app/controllers/api/v1/accounts/deals_controller.rb
+++ app/controllers/api/v1/accounts/deals_controller.rb
@@ -21,7 +21,7 @@ class Api::V1::Accounts::DealsController < Api::V1::Accounts::BaseController
                          .first
     attributes[:probability] = stage.default_probability if stage && attributes[:probability].blank?
     @deal = Current.account.deals.create!(attributes)
-    create_event!('deal_created', to: deal_snapshot(@deal))
+    create_event!('deal_created', { to: deal_snapshot(@deal) })
   end
 
   def update
@@ -61,14 +61,14 @@ class Api::V1::Accounts::DealsController < Api::V1::Accounts::BaseController
   end
 
   def log_change_events(before, after)
-    create_event!('stage_changed', from: before['pipeline_stage_id'], to: after['pipeline_stage_id']) if before['pipeline_stage_id'] != after['pipeline_stage_id']
-    create_event!('status_changed', from: before['status'], to: after['status']) if before['status'] != after['status']
-    create_event!('assignment_changed', from: before['assignee_id'], to: after['assignee_id']) if before['assignee_id'] != after['assignee_id']
+    create_event!('stage_changed', { from: before['pipeline_stage_id'], to: after['pipeline_stage_id'] }) if before['pipeline_stage_id'] != after['pipeline_stage_id']
+    create_event!('status_changed', { from: before['status'], to: after['status'] }) if before['status'] != after['status']
+    create_event!('assignment_changed', { from: before['assignee_id'], to: after['assignee_id'] }) if before['assignee_id'] != after['assignee_id']
 
     ignored_keys = %w[pipeline_stage_id status assignee_id]
     before_other = before.except(*ignored_keys)
     after_other = after.except(*ignored_keys)
-    create_event!('deal_updated', from: before_other, to: after_other) if before_other != after_other
+    create_event!('deal_updated', { from: before_other, to: after_other }) if before_other != after_other
   end
 
   def create_event!(event_type, metadata)
